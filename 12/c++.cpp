84cc83d23958bb400b635e041fe7415af7dd87bc
anuragmor <500069361@stu.upes.ac.in>
2019-03-18 10:54:09 +0530

    main body included

--- 12/c++.cpp
+++ 12/c++.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 using namespace std;
 int main ()
+{
+	int a=5 ,b=10 ,temp;
+	cout << "before swapping."<<end1;
+	temp=a;
+	a=b;
+	b=temp;
+	cout <<'\nafter swapping.",, endl;
+	cout <<"a="<<a<<",b=<<B<<end1;
+
